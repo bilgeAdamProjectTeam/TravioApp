2e0f24c7237499c9f7efe61ee0b420b4581d111c
kubra demirkaya <kubrademirkaya3@gmail.com>
2023-09-11 20:22:26 +0300

    LoginVC refactor

--- TravioApp/Application/Login/LoginVC.swift
+++ TravioApp/Application/Login/LoginVC.swift
@@ -9,8 +9,8 @@ import SnapKit
 import UIKit
 
 class LoginVC: UIViewController {
-    // login view model instance
-    private lazy var LoginViewModelInstance: LoginViewModel = {
+    
+    private lazy var viewModel: LoginViewModel = {
         let view = LoginViewModel()
         return view
     }()
@@ -18,13 +18,23 @@ class LoginVC: UIViewController {
     private lazy var logo: UIImageView = {
         let logo = UIImageView()
         logo.image = UIImage(named: "travioLogo")
-        
         return logo
     }()
     
     private lazy var retangle: UIView = {
         let view = CustomView()
-        
+        return view
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.backgroundColor = .clear
+        scrollView.isScrollEnabled = true
+        return scrollView
+    }()
+    
+    private lazy var scrollContentView: UIView = {
+        let view = UIView()
         return view
     }()
     
@@ -33,7 +43,6 @@ class LoginVC: UIViewController {
         label.text = "Welcome to Travio"
         label.font =  Font.medium(size: 24).font
         label.textColor = Color.darkGray.color
-        
         return label
     }()
     
@@ -42,8 +51,6 @@ class LoginVC: UIViewController {
         view.labelText = "Email"
         view.placeholderName = "bilgeadam@gmail.com"
         view.txtField.text = "t@gmail.com"
-        
-        
         return view
     }()
     
@@ -53,80 +60,113 @@ class LoginVC: UIViewController {
         view.placeholderName = "*********"
         view.txtField.text = "12345678"
         view.txtField.isSecureTextEntry = true
-        
         return view
     }()
     
     private lazy var loginButton: UIButton = {
         let btn = CustomButton()
         btn.labelText = "Login"
-        
         btn.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         return btn
     }()
     
-    private lazy var forgotStackView: UIStackView = {
+    private lazy var stackViewSignUp: UIStackView = {
         let stack = UIStackView()
-        
         return stack
     }()
     
-    private lazy var accLabel: UILabel = {
+    private lazy var labelSignUp: UILabel = {
         let label = UILabel()
         label.text = "Don't have any account?"
         label.font = Font.semiBold(size: 14).font
-        
         return label
     }()
     
-    private lazy var signUp: UIButton = {
+    private lazy var buttonSignUp: UIButton = {
         let button = UIButton()
         button.setTitle(" Sign Up", for: .normal)
         button.titleLabel?.font =  Font.semiBold(size: 14).font
         button.setTitleColor(.black, for: .normal)
-        
         button.addTarget(self, action: #selector(signUpTapped), for: .touchUpInside)
         return button
         
     }()
     
+    @objc func signUpTapped() {
+        
+        let signUpVC = SignUpVC()
+        navigationController?.pushViewController(signUpVC, animated: true)
+    }
+    
+    @objc func loginButtonTapped() {
+        
+        guard let email = txtMailView.txtField.text,
+              let password = txtPasswordView.txtField.text else { return }
+        
+        let data = LoginInfo(email: email, password: password)
+        
+        viewModel.login(input: data)
+        
+        let tabBarController = TabBarController()
+        self.navigationController?.pushViewController(tabBarController, animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupViews()
     }
     
     func setupViews() {
-        view.backgroundColor =  Color.turquoise.color
-        view.addSubview(logo)
-        view.addSubview(retangle)
-        retangle.addSubview(welcomeLabel)
-        retangle.addSubview(txtMailView)
-        retangle.addSubview(txtPasswordView)
-        retangle.addSubview(loginButton)
-        retangle.addSubview(forgotStackView)
-        forgotStackView.addArrangedSubview(accLabel)
-        forgotStackView.addArrangedSubview(signUp)
-        setupLayouts()
-    }
-    
-    func setupLayouts() {
+        
+        self.view.backgroundColor = Color.turquoise.color
+        
         navigationController?.isNavigationBarHidden = true
         
-        logo.snp.makeConstraints { make in
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(44)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(178)
-            make.width.equalTo(150)
-        }
+        self.view.addSubviews(retangle,
+                              logo)
+        
+        retangle.addSubview(scrollView)
+        
+        scrollView.addSubview(scrollContentView)
+        
+        scrollContentView.addSubviews(welcomeLabel,
+                                      txtMailView,
+                                      txtPasswordView,
+                                      loginButton,
+                                      stackViewSignUp)
+        
+        stackViewSignUp.addArrangedSubviews(labelSignUp,
+                                            buttonSignUp)
+        
+        setupLayout()
+    }
+    
+    func setupLayout() {
         
         retangle.snp.makeConstraints { make in
-            make.top.equalTo(logo.snp.bottom).offset(24)
-            make.leading.trailing.bottom.equalToSuperview().offset(0)
+            make.top.equalToSuperview().offset(246)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        logo.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(50)
+            make.centerX.equalToSuperview()
+        }
+        
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        scrollContentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(self.view)
+            make.bottom.equalTo(stackViewSignUp.snp.bottom).offset(16)
         }
         
         welcomeLabel.snp.makeConstraints { make in
-            make.top.equalTo(retangle.snp.top).offset(64)
+            make.top.equalToSuperview().offset(64)
             make.centerX.equalToSuperview()
         }
         
@@ -134,49 +174,26 @@ class LoginVC: UIViewController {
             make.top.equalTo(welcomeLabel.snp.bottom).offset(41)
             make.leading.equalToSuperview().offset(24)
             make.trailing.equalToSuperview().offset(-24)
-            make.height.equalTo(74)
         }
-        
+
         txtPasswordView.snp.makeConstraints { make in
             make.top.equalTo(txtMailView.snp.bottom).offset(24)
             make.leading.equalToSuperview().offset(24)
             make.trailing.equalToSuperview().offset(-24)
-            make.height.equalTo(74)
         }
-        
+
         loginButton.snp.makeConstraints { make in
             make.top.equalTo(txtPasswordView.snp.bottom).offset(48)
             make.leading.equalToSuperview().offset(24)
             make.trailing.equalToSuperview().offset(-24)
-            make.height.equalTo(54)
         }
-        
-        forgotStackView.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-21)
-            make.centerX.equalToSuperview()
-        }
-    }
-    
-    @objc func signUpTapped() {
-        let vc = SignUpVC()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    @objc func loginButtonTapped() {
-        guard let email = txtMailView.txtField.text, let password = txtPasswordView.txtField.text else { return }
 
-        
-        let data = LoginInfo(email: email, password: password)
-      
-        LoginViewModelInstance.login(input: data)
-        let tabBarController = TabBarController()
-        self.navigationController?.pushViewController(tabBarController, animated: true)
-
-            
-            
+        stackViewSignUp.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(141)
+            make.leading.equalToSuperview().offset(74)
         }
-       
     }
+}
 
 
 
